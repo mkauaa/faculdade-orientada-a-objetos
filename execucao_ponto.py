448bcc99e1f5ce15b24e49da78a7b944ae5b9043
Kauã Andrade <kauaa.matos@gmail.com>
2022-09-26 19:45:55 -0300

    Update execucao_ponto.py

--- execucao_ponto.py
+++ execucao_ponto.py
@@ -2,8 +2,14 @@ from classe_ponto import Circunferencia, Ponto
 
 keke = Ponto(2, 3, 'Verde')
 
+keke.alterar_cor('Azul')
+
 dako = Circunferencia(keke, 5, 'Vermelho', 'Roxo')
+
+dako.perimetro()
 
 dako.imprimir()
 
+print(f'A área equivale a {dako.area()} e o perímetro equivale a {dako.perimetro()}.')
 
+print()

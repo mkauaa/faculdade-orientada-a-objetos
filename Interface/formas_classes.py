6afeb8ff1bf9e28584eed86393ce68c81702208f
Kauã Andrade <kauaa.matos@gmail.com>
2022-12-20 15:04:03 -0300

    .

--- Interface/formas_classes.py
+++ Interface/formas_classes.py
@@ -1,4 +1,24 @@
-class Poliedro:
+from iformageometrica import iFormaGeometrica
+import math
+
+
+class Cilindro(iFormaGeometrica):
+    def __init__(self, raio, altura):
+        self.raio = raio
+        self.altura = altura
+
+    def calculaArea(self):
+        area = 2 * math.pi * self.raio * (self.altura + self.raio)
+        return area
+
+    def calculaVolume(self):
+        volume = math.pi * self.raio * self.raio * self.altura
+        return volume
+
+    def __str__(self):
+        return f'Area: {self.calculaArea}\n Volume: {self.calculaVolume}'
+
+class Poliedro(iFormaGeometrica):
     def __init__(self, vertices, arestas, faces, l1, l2, altura):
         self.vertices = vertices
         self.arestas = arestas
@@ -22,17 +42,52 @@ class Poliedro:
             
         return s
 
-class Cilindro:
-    def __init__(self, raio, altura):
-        self.raio = raio
-        self.altura = altura
+class Cubo(Poliedro):
+    def __init__(self, lado):
+        super().__init__(self)
+        self.lado = lado
+        self.vertices = 12
+        self.arestas = 8
+        self.faces = 6
 
     def calculaArea(self):
-        return {2 * 3.14 * self.raio(self.altura + self.raio)}
+        area = 6 * (math.pow(self.lado, 2))
+        return area
 
     def calculaVolume(self):
-        return {3,14 * self.raio * self.raio * self.altura}
+        volume = math.pow(self.lado, 3)
+        return volume
 
-    def __str__(self):
-        return f'Area: {self.calculaArea}\n Volume: {self.calculaVolume}'
+class Retangulo(Poliedro):
+    def __init__(self, altura, comprimento, largura):
+        super().__init__(self, altura)
+        self.altura = altura
+        self.comprimento = comprimento
+        self.largura = largura
+        self.vertices = 12
+        self.arestas = 8
+        self.faces = 6
+
+    def calculaArea(self):
+        area = 2 * (self.largura * self.comprimento + self.comprimento * self.altura + self.largura * self.comprimento)
+        return area
+
+    def calculaVolume(self):
+        volume = self.altura * self.comprimento * self.largura
+        return volume
+
+
+# PRINCIPAL #
+
+iFormaGeometrica.register(Poliedro)
+iFormaGeometrica.register(Cilindro)
+
+cubo = Cubo(5)
+print(cubo)
+
+retangulo = Retangulo(2, 3, 4)
+print(retangulo)
+
+cilindro = Cilindro(2, 5)
+print(cilindro)
 
